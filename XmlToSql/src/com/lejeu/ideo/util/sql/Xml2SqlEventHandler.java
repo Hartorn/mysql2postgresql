038c30cee47edb50b4dc4ca5c1c1efb145917be0
Hartorn <bazirehoussin@gmail.com>
2015-11-19 09:30:19 +0100

    First version, to migrate data

---
+++ XmlToSql/src/com/lejeu/ideo/util/sql/Xml2SqlEventHandler.java
@@ -0,0 +1,205 @@
+/**
+ *
+ */
+package com.lejeu.ideo.util.sql;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.Writer;
+import java.text.MessageFormat;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Properties;
+import java.util.Set;
+
+import org.xml.sax.Attributes;
+import org.xml.sax.SAXException;
+import org.xml.sax.helpers.DefaultHandler;
+
+/**
+ * @author Bazire
+ *
+ */
+public class Xml2SqlEventHandler extends DefaultHandler {
+	protected enum DbTypes {
+		VARCHAR("character varying"),
+
+		BIT("bit"),
+
+		BIGINT("bigint"),
+
+		CHAR("character"),
+
+		SMALLINT("smallint"),
+
+		INT("integer"), REAL("real"),
+
+		TIMESTAMP("timestamp without time zone"),
+
+		TEXT("text");
+
+		public static DbTypes getEnumEltByDbType(final String dbType) throws SAXException {
+			for (final DbTypes type : DbTypes.values()) {
+				if (type.dbType.equals(dbType)) {
+					return type;
+				}
+			}
+			throw new SAXException("Missing enum type for db type:" + dbType);
+		}
+
+		private final String dbType;
+
+		private DbTypes(final String type) {
+			this.dbType = type;
+		}
+
+		public String formatForSql(final String value) throws SAXException {
+			String formattedValue;
+			switch (this) {
+			case BIT:
+				formattedValue = value + "::bit";
+				break;
+			case VARCHAR:
+			case CHAR:
+			case TEXT:
+			case TIMESTAMP:
+				formattedValue = "'" + value.replaceAll("'", "''") + "'";
+				break;
+			case SMALLINT:
+			case BIGINT:
+			case INT:
+			case REAL:
+				formattedValue = value;
+				break;
+			default:
+				throw new SAXException("Missing enum type:" + name());
+			}
+			return formattedValue;
+		}
+	}
+
+	final private Properties dbMapping;
+	final private String SQL_TEMPLATE = "INSERT INTO {0} ({1}) VALUES ({2});\n";
+	final private Writer writer;
+	final private Map<String, String> attributesMap = new HashMap<>();
+	private String tableName = null;
+	private String fieldName = null;
+
+	public Xml2SqlEventHandler(final Writer writer) throws IOException {
+		this.writer = writer;
+
+		final Properties prop = new Properties();
+		try (InputStream input = getClass().getResourceAsStream("columnMapping.properties");
+				Reader inputReader = new InputStreamReader(input, XmlToSql.CHARSET);) {
+			prop.load(inputReader);
+		}
+		this.dbMapping = prop;
+	}
+
+	@Override
+	public void characters(final char[] ch, final int start, final int length) throws SAXException {
+		if (this.fieldName != null) {
+			this.attributesMap.put(this.fieldName, String.copyValueOf(ch, start, length));
+		}
+	}
+
+	@Override
+	public void endDocument() throws SAXException {
+		try {
+			endSql();
+		} catch (final IOException e) {
+			throw new SAXException(e);
+		}
+	}
+
+	@Override
+	public void endElement(final String uri, final String localName, final String qName) throws SAXException {
+		switch (localName) {
+		case "table_data":
+			this.tableName = null;
+			break;
+		case "row":
+			try {
+				writeSql();
+			} catch (final IOException e) {
+				throw new SAXException(e);
+			}
+			this.attributesMap.clear();
+			break;
+		case "field":
+			this.fieldName = null;
+			break;
+		default:
+			break;
+		}
+
+	}
+
+	private void endSql() throws IOException {
+		this.writer.write("\n\ncommit;");
+	}
+
+	private String prepareStringValueForSql(final String attrName, final String value) throws SAXException {
+		// System.out.println("Clef:" + this.tableName + "." + attrName);
+		// System.out.println("Property:" + this.dbMapping.getProperty(this.tableName + "." + attrName));
+		// System.out.println();
+		final DbTypes type = DbTypes
+				.getEnumEltByDbType(this.dbMapping.getProperty((this.tableName + "." + attrName).toLowerCase()));
+		return type.formatForSql(value);
+	}
+
+	@Override
+	public void startDocument() throws SAXException {
+		try {
+			startSql();
+		} catch (final IOException e) {
+			throw new SAXException(e);
+		}
+	}
+
+	@Override
+	public void startElement(final String uri, final String localName, final String qName, final Attributes attributes)
+			throws SAXException {
+		switch (localName) {
+		case "table_data":
+			this.tableName = attributes.getValue(attributes.getIndex("name"));
+			break;
+		case "field":
+			this.fieldName = attributes.getValue(attributes.getIndex("name"));
+			break;
+		default:
+			break;
+		}
+	}
+
+	private void startSql() throws IOException {
+		this.writer.write("SET CLIENT_ENCODING TO '" + XmlToSql.CHARSET + "';\n\n");
+		this.writer.write("start transaction;\n\n");
+	}
+
+	private void writeSql() throws IOException, SAXException {
+		final StringBuilder attrNames = new StringBuilder();
+		final StringBuilder attrValues = new StringBuilder();
+
+		final Set<Entry<String, String>> row = this.attributesMap.entrySet();
+		for (final Entry<String, String> entry : row) {
+			attrNames.append(entry.getKey()).append(',');
+		}
+		if (attrNames.length() > 0) {
+			attrNames.setLength(attrNames.length() - 1);
+		}
+
+		for (final Entry<String, String> entry : row) {
+			attrValues.append(prepareStringValueForSql(entry.getKey(), entry.getValue())).append(',');
+		}
+		if (attrValues.length() > 0) {
+			attrValues.setLength(attrValues.length() - 1);
+		}
+
+		this.writer.write(
+				MessageFormat.format(this.SQL_TEMPLATE, this.tableName, attrNames.toString(), attrValues.toString()));
+	}
+}
---
+++ XmlToSql/src/com/lejeu/ideo/util/sql/XmlToSql.java
@@ -0,0 +1,136 @@
+/**
+ *
+ */
+package com.lejeu.ideo.util.sql;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.Reader;
+import java.io.Writer;
+
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.parsers.SAXParser;
+import javax.xml.parsers.SAXParserFactory;
+
+import org.xml.sax.InputSource;
+import org.xml.sax.SAXException;
+
+/**
+ * @author Bazire
+ *
+ */
+public class XmlToSql {
+	private static final int BUFFER_SIZE = 1024 * 8;
+	public static final String CHARSET = "ISO-8859-1";
+
+	private static File copyToCorrectedFile(final String filename) throws IOException {
+		final File xmlFile = new File(filename);
+		final File xmlFileCorrected = new File(filename + ".corrected");
+		if (xmlFileCorrected.exists()) {
+			xmlFileCorrected.createNewFile();
+		}
+		final char[] buffer = new char[XmlToSql.BUFFER_SIZE];
+		int nbChars;
+
+		try (final OutputStream outputStream = new BufferedOutputStream(new FileOutputStream(xmlFileCorrected));
+				final InputStream inputStream = new BufferedInputStream(new FileInputStream(xmlFile),
+						XmlToSql.BUFFER_SIZE);
+				final Reader reader = new InputStreamReader(inputStream, XmlToSql.CHARSET);
+				final Writer writer = new OutputStreamWriter(outputStream, XmlToSql.CHARSET);) {
+			while ((nbChars = reader.read(buffer)) != -1) {
+				writer.write(XmlToSql.stripNonValidXMLCharacters(String.copyValueOf(buffer, 0, nbChars)));
+			}
+		}
+		return xmlFileCorrected;
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(final String[] args) {
+		String filename = null;
+
+		for (int i = 0; i < args.length; i++) {
+			filename = args[i];
+			if (i != (args.length - 1)) {
+				XmlToSql.usage();
+			}
+		}
+
+		if (filename == null) {
+			XmlToSql.usage();
+		}
+
+		final File sqlFile = new File(filename + ".sql");
+
+		File xmlFileCorrected = null;
+		try {
+			xmlFileCorrected = XmlToSql.copyToCorrectedFile(filename);
+		} catch (final IOException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+
+		try (final InputStream xmlStream = new BufferedInputStream(new FileInputStream(xmlFileCorrected),
+				XmlToSql.BUFFER_SIZE);
+				final Reader xmlReader = new InputStreamReader(xmlStream, XmlToSql.CHARSET);
+				final OutputStream outputStream = new FileOutputStream(sqlFile);
+				final Writer writer = new OutputStreamWriter(outputStream, XmlToSql.CHARSET);) {
+
+			final InputSource is = new InputSource(xmlReader);
+			is.setEncoding(XmlToSql.CHARSET);
+			final SAXParserFactory spf = SAXParserFactory.newInstance();
+			spf.setNamespaceAware(true);
+			final SAXParser saxParser = spf.newSAXParser();
+			saxParser.parse(is, new Xml2SqlEventHandler(writer));
+
+		} catch (final IOException | SAXException | ParserConfigurationException e) {
+			e.printStackTrace();
+			System.exit(1);
+		}
+	}
+
+	/**
+	 * This method ensures that the output String has only
+	 * valid XML unicode characters as specified by the
+	 * XML 1.0 standard. For reference, please see
+	 * <a href="http://www.w3.org/TR/2000/REC-xml-20001006#NT-Char">the
+	 * standard</a>. This method will return an empty
+	 * String if the input is null or empty.
+	 *
+	 * @param in
+	 *            The String whose non-valid characters we want to remove.
+	 * @return The in String, stripped of non-valid characters.
+	 */
+	private static String stripNonValidXMLCharacters(final String in) {
+		final StringBuffer out = new StringBuffer(); // Used to hold the output.
+		char current; // Used to reference the current character.
+
+		if ((in == null) || ("".equals(in))) {
+			return ""; // vacancy test.
+		}
+		for (int i = 0; i < in.length(); i++) {
+			current = in.charAt(i); // NOTE: No IndexOutOfBoundsException caught here; it should not happen.
+			if ((current == 0x9) || (current == 0xA) || (current == 0xD) || ((current >= 0x20) && (current <= 0xD7FF))
+					|| ((current >= 0xE000) && (current <= 0xFFFD))
+					|| ((current >= 0x10000) && (current <= 0x10FFFF))) {
+				out.append(current);
+			}
+		}
+		return out.toString();
+	}
+
+	private static void usage() {
+		System.err.println("Usage: XmlToSql <file.xml>");
+		System.err.println("       -usage or -help = this message");
+		System.exit(1);
+	}
+}
